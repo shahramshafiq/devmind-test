0b543161ed6b64e0df2e10afb5595411fa5fea36
Shahram Shafiq <106769228+shahramshafiq@users.noreply.github.com>
2026-06-22 01:28:05 +0500

    devmind: update test_pagination.py

--- test_pagination.py
+++ test_pagination.py
@@ -2,28 +2,29 @@ import unittest
 from pagination import paginate
 
 class TestPaginateFunction(unittest.TestCase):
+    def test_list_length_exactly_divisible_by_page_size(self):
+        list_to_paginate = [1, 2, 3, 4, 5, 6]
+        page_size = 3
+        pages = paginate(list_to_paginate, page_size)
+        self.assertEqual(pages, [[1, 2, 3], [4, 5, 6]])
+
+    def test_list_length_not_exactly_divisible_by_page_size(self):
+        list_to_paginate = [1, 2, 3, 4, 5, 6, 7]
+        page_size = 3
+        pages = paginate(list_to_paginate, page_size)
+        self.assertEqual(pages, [[1, 2, 3], [4, 5, 6], [7]])
+
     def test_empty_list(self):
-        self.assertEqual(paginate([], 10), [])
+        list_to_paginate = []
+        page_size = 3
+        pages = paginate(list_to_paginate, page_size)
+        self.assertEqual(pages, [])
 
-    def test_list_length_divisible_by_page_size(self):
-        self.assertEqual(paginate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10), [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+    def test_page_size_larger_than_list_length(self):
+        list_to_paginate = [1, 2, 3]
+        page_size = 5
+        pages = paginate(list_to_paginate, page_size)
+        self.assertEqual(pages, [[1, 2, 3]])
 
-    def test_list_length_not_divisible_by_page_size(self):
-        self.assertEqual(paginate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 10), [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [11]])
-
-    def test_page_size_greater_than_list_length(self):
-        self.assertEqual(paginate([1, 2, 3], 10), [[1, 2, 3]])
-
-    def test_page_size_of_1(self):
-        self.assertEqual(paginate([1, 2, 3], 1), [[1], [2], [3]])
-
-    def test_invalid_input(self):
-        with self.assertRaises(ValueError):
-            paginate("not a list", 10)
-        with self.assertRaises(ValueError):
-            paginate([1, 2, 3], "not an integer")
-        with self.assertRaises(ValueError):
-            paginate([1, 2, 3], -10)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
